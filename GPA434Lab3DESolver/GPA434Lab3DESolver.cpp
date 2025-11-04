00da4fef74483ed3c502b9b52f0dc8c603b25bb1
Alexandre Chapleau <alexandrechapleau@hotmail.com>
2025-11-04 18:09:55 -0500

    f

---
+++ GPA434Lab3DESolver/GPA434Lab3DESolver.cpp
@@ -0,0 +1,16 @@
+#include "GPA434Lab3DESolver.h"
+
+GPA434Lab3DESolver::GPA434Lab3DESolver(QWidget *parent)
+    : QMainWindow(parent)
+{
+    setWindowTitle("Differential Evolution Solver");
+    setWindowIcon(QIcon(":/GPA434Lab3DESolver/dna-icon"));
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // ajouter votre code à partir d'ici...
+    // ...
+}
+
+GPA434Lab3DESolver::~GPA434Lab3DESolver()
+{
+}
